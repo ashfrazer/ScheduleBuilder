0fb21b52ef25d3cc9ce1f7c86af65b28f2c48d7c
ashfrazer <afrazer@cub.uca.edu>
2025-03-11 13:30:02 -0500

    Spring Boot Template

---
+++ src/main/java/edu/uca/sdc/schedulebuilder/ScheduleBuilderApplication.java
@@ -0,0 +1,13 @@
+package edu.uca.sdc.schedulebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uca/sdc/schedulebuilder/ScheduleBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uca.sdc.schedulebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
